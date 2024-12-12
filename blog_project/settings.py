e0a1a5abb4eb75e2b731ce096781848aa39e24d0
Parallel-EDU <hello@paralleledu.com>
2024-12-12 12:45:19 +0530

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -27,7 +27,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middelware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'blog_project.urls'
